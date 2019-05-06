479448b44b098661af0db6768193356ed56ec0cf
Jean Prunneaux <jean@prunneaux.com>
2019-05-06 18:15:19 +0200

    Improve files root test

--- 03_create_environment.sh
+++ 03_create_environment.sh
@@ -12,61 +12,63 @@ tb_test -n "Create source test directory" mkdir -p "$src" || return 1
 tb_test -n "Create destination test directory" mkdir -p "$dest" || return 1
 
 # create source files
-# details:
 #   /
 #     /dir1/
 #       /subdir1/
 #         /file1
-#         /file2
-#       /subdir2/
+#         ...
+#         /file9
+#       ...
+#       /subdir9/
 #         /file1
-#         /file2
-#     /dir2/
+#         ...
+#         /file9
+#     ...
+#     /dir9/
 #       /subdir1/
 #         /file1
-#         /file2
-#       /subdir2/
+#         ...
+#         /file9
+#       ...
+#       /subdir9/
 #         /file1
-#         /file2
+#         ...
+#         /file9
+# variables:
+#   $dir1: path to dir1
+#   $dir11: path to subdir1 in dir1
+#   $file111: path to file1 in subdir1 in dir1
+#   ...
+#   $file999: path to file9 in subdir9 in dir9
 
-dir1=$src/dir1/
-dir2=$src/dir2/
+echo
+echo "Create files..."
 
-dir11=$dir1/subdir1/
-mkdir -p "$dir11"
+# create main directories
+for d in $(seq 1 9) ; do
+	dir=$src/dir$d
+	eval dir$d=\"$dir\"
 
-dir12=$dir1/subdir2/
-mkdir -p "$dir12"
+	# create subdirectories
+	for s in $(seq 1 9) ; do
+		subdir=$dir/subdir$s
+		eval dir$d$s=\"$subdir\"
 
-dir21=$dir2/subdir1/
-mkdir -p "$dir21"
+		mkdir -p "$subdir"
 
-dir22=$dir2/subdir2/
-mkdir -p "$dir22"
+		# create files
+		for f in $(seq 1 9) ; do
+			file=$subdir/file$f
+			eval file$d$s$f=\"$file\"
 
-file111=$dir11/file1
-echo "I am a file" > "$file111"
-file112=$dir11/file2
-echo "I am a file" > "$file112"
-
-file121=$dir12/file1
-echo "I am a file" > "$file121"
-file122=$dir12/file2
-echo "I am a file" > "$file122"
-
-file211=$dir21/file1
-echo "I am a file" > "$file211"
-file212=$dir21/file2
-echo "I am a file" > "$file212"
-
-file221=$dir22/file1
-echo "I am a file" > "$file221"
-file222=$dir22/file2
-echo "I am a file" > "$file222"
+			echo "$file" > "$file"
+		done
+	done
+done
 
 # hard link
-ln "$src/file111" "$src/file111-hardlink"
+ln "$file111" "$src"/file111-hardlink
 
 # symlinks
-ln -s "$src/dir1" "$src/dir1-symlink"
-ln -s "$src/file111" "$src/file111-symlink"
+ln -s "$src"/dir1 "$src"/dir1-symlink
+ln -s "$file111" "$src"/file111-symlink
