e4c113bea9415b852a6422b9063256c7129e3a50
Jean Prunneaux <jean@prunneaux.com>
2020-04-03 16:44:02 +0200

    Improve mv test

--- 13_test_mv.sh
+++ 13_test_mv.sh
@@ -2,6 +2,6 @@
 # Usage: test_mv [OPTIONS]
 # Dependencies: $conf
 function test_mv() {
-	test_t2b "$@" "$conf: mv file112 file999" mv -f "$file112" "$dir11"/file999 && \
-	test_t2b "$@" "$conf: file999 exists" history -q "$dir11"/file999
+	test_t2b "$@" "$conf: mv file112 file999" mv -f "$file112" "$dir11"/to/new/file999 && \
+	test_t2b "$@" "$conf: file999 exists" history -q "$dir11"/to/new/file999
 }
