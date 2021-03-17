1ebcfb77b23929ff4126db3ea2cc17a5f44c8c28
Jean Prunneaux <jean@prunneaux.com>
2021-03-18 00:31:13 +0100

    Improve init & config

--- 03_create_environment.sh
+++ 03_create_environment.sh
@@ -1,5 +1,23 @@
 # Init: set environment
 
+# load libbash
+source "$t2b_path"/libbash/libbash.sh - || return 1
+
+# test if time2backup exists
+tb_test -n "Test if time2backup is loaded" [ -f "$t2b_path/time2backup.sh" ] || return 1
+
+curdir=$(dirname "$BASH_SOURCE")
+config_directory=$curdir/config
+testdir="$(lb_realpath "$curdir")/t2b test files"
+
+# time2backup config version
+config_version=1.9.0
+
+# time2backup default options
+console_mode=true
+debug_mode=false
+quiet_mode=true
+
 src=$testdir/src
 dest=$testdir/dest
 
